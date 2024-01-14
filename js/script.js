93d488814e3d4637feea0ca32a0518694a5d15f1
mikecat08 <kyorokyoro0813@gmail.com>
2024-01-14 21:31:23 +0900

    $マークをjQueryに置換
    WordPressに流し込む準備として、$を全てjQueryに置換しました。

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-$(function () {
+jQuery(function () {
 
   //-------------------------------------
   // fade upの動き
   //-------------------------------------
 
   //ウィンドウがスクロールされたタイミングで下記の処理が動きます。
-  $(window).scroll(function () {
+  jQuery(window).scroll(function () {
 
     //ページ上端からウィンドウ上端までの距離を取得します。
-    var scrollTop = $(this).scrollTop();
+    var scrollTop = jQuery(this).scrollTop();
 
     //ページ上端からウィンドウ下端までの距離を取得します。
-    var scrollBottom = scrollTop + $(this).height();
+    var scrollBottom = scrollTop + jQuery(this).height();
 
     //fadeup-targetというクラスを持つ要素それぞれに下記の処理を行います。
     //.each()が無いと全部のfadeup-targetクラスに対して一気に処理が行われてしまいます。
-    $('.fadeup-target').each(function () {
+    jQuery('.fadeup-target').each(function () {
 
       //ウィンドウ下端に対象要素が250px分見えたタイミングで、in-viewというクラスが付与されます。
-      if (scrollBottom > $(this).offset().top + 250) {
-        $(this).addClass('in-view');
+      if (scrollBottom > jQuery(this).offset().top + 250) {
+        jQuery(this).addClass('in-view');
         //in-viewというクラスが付与されると、cssのアニメーションが動き始めます。
       }
     });
@@ -31,41 +31,41 @@ $(function () {
   //-------------------------------------
 
   //何度も指定するのでボタンとメニューの要素指定を変数に格納します。
-  var $burgerBtn = $('#burger-btn');
-  var $burgerMenu = $('#burger-menu');
+  var jQueryburgerBtn = jQuery('#burger-btn');
+  var jQueryburgerMenu = jQuery('#burger-menu');
 
   //ハンバーガーボタンをクリックした時の処理
-  $burgerBtn.on('click', function () {
+  jQueryburgerBtn.on('click', function () {
 
     //ハンバーガーメニューがopenedというクラスを持っている場合
-    if ($burgerMenu.hasClass('opened')) {
+    if (jQueryburgerMenu.hasClass('opened')) {
 
       //ハンバーガーメニューからopenedというクラスを削除します。
-      $burgerMenu.removeClass('opened');
+      jQueryburgerMenu.removeClass('opened');
       
       //暗くなっている背景を元に戻す目的で、dark-maskというidを持つ要素からactiveというクラスを取り除きます。
-      $('#dark-mask').removeClass('active');
+      jQuery('#dark-mask').removeClass('active');
       
       //ハンバーガーボタンからactiveというクラスを削除します。
-      $burgerBtn.removeClass('active');
+      jQueryburgerBtn.removeClass('active');
 
       //ボタンの中のテキストを取得して書き換えます。
-      $burgerBtn.children('span').text('メニュー');
+      jQueryburgerBtn.children('span').text('メニュー');
 
       //ハンバーガーメニューがopenedというクラスを持っていない場合
     } else {
 
       //ハンバーガーメニューにopenedというクラスを付与します。
-      $burgerMenu.addClass('opened');
+      jQueryburgerMenu.addClass('opened');
       
       //背景を暗くする目的で、bgというidを持つ要素にactiveというクラスを付与します。
-      $('#dark-mask').addClass('active');
+      jQuery('#dark-mask').addClass('active');
       
       //ハンバーガーボタンにactiveというクラスを付与します。
-      $burgerBtn.addClass('active');
+      jQueryburgerBtn.addClass('active');
 
       //ボタンの中のテキストを取得して書き換えます。
-      $burgerBtn.children('span').text('閉じる');
+      jQueryburgerBtn.children('span').text('閉じる');
     }
   });
 
